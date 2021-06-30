dc0867994b6fe54e1d4dcbebe3a763fbcac27aef
bigtittygothgirlfriend <eugen.haidarli@gmail.com>
2021-06-30 19:11:38 +0300

    Added only the Anime Viewset to test the site

--- anime/views.py
+++ anime/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Anime, Comment, Rating
+from .serializers import AnimeSerializer, CommentSerializer, RatingSerializer
+from rest_framework import permissions
 
-# Create your views here.
+class AnimeViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Anime.objects.all()
+    serializer_class = AnimeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
