bd6b661fdfac9f8a0f820957d0988b235d7f24c9
bigtittygothgirlfriend <eugen.haidarli@gmail.com>
2021-06-30 20:00:19 +0300

    Added UserSerializer and also id field to every Serializer

--- anime/serializers.py
+++ anime/serializers.py
@@ -1,22 +1,34 @@
 from rest_framework import serializers
 from .models import Anime, Comment, Rating
+from users.models import CustomUser
 
-class AnimeSerializer(serializers.ModelSerializer):
-
-
-    class Meta:
-        model = Anime
-        fields = ['name', 'owner', 'is_series', 'episodes']
 
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['anime', 'owner', 'time', 'content']
+        fields = ['id', 'anime', 'owner', 'time', 'content']
+
 
 class RatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rating
-        fields = ['anime', 'owner', 'score']
+        fields = ['id', 'anime', 'owner', 'score']
 
+
+class AnimeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Anime
+        fields = ['id', 'name', 'owner', 'is_series', 'episodes']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(read_only = True, many=True)
+    ratings = RatingSerializer(read_only=True, many=True)
+    animes = AnimeSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'animes', 'comments', 'ratings']
