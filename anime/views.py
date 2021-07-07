aec29dfc7a2014e744445c0e81ae1a9a927857e2
bigtittygothgirlfriend <eugen.haidarli@gmail.com>
2021-07-07 21:53:28 +0300

    Added actions for all the managers

--- anime/views.py
+++ anime/views.py
@@ -1,3 +1,4 @@
+from django.db.models import manager, query
 from rest_framework import serializers, viewsets
 from .models import Anime, Comment, Rating
 from .serializers import AnimeSerializer, CommentSerializer, RatingSerializer, UserSerializer
@@ -5,6 +6,8 @@ from rest_framework import permissions
 from users.models import CustomUser
 from rest_framework.exceptions import PermissionDenied
 from django.http import HttpResponseBadRequest
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class AnimeViewSet(viewsets.ModelViewSet):
     """
@@ -20,6 +23,30 @@ class AnimeViewSet(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         return HttpResponseBadRequest("We dont delete art around this parts >:(")
 
+    @action(detail=False, methods=['get'], url_path='rated')
+    def top_rated_animes(self, request):
+        query = self.queryset.get_top_anime()
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='last')
+    def last_made_anime(self, request, size=5):
+        query = self.queryset.get_latest_anime(size)
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='country')
+    def anime_by_country(self, request, country="japan"):
+        query = self.queryset.get_anime_by_country(country)
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='watched')
+    def top_watched_anime(self, request):
+        query = self.queryset.get_top_watched_anime()
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     User viewset
@@ -28,6 +55,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=False, methods=['get'], url_path='top')
+    def top_users(self, request):
+        query = self.queryset.get_top_users()
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -42,6 +75,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    @action(detail=False, methods=['get'], url_path='latest')
+    def latest_comment(self, request, size=5):
+        query = self.queryset.get_latest_comments(size)
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
+
 class RatingViewSet(viewsets.ModelViewSet):
     """
     Anime Ratings viewset
@@ -53,5 +92,11 @@ class RatingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False, methods=['get'], url_path='score')
+    def anime_by_score(self, request, score='masterpiece'):
+        query = self.queryset.get_anime_by_rating(score)
+        serializer = self.serializer_class(query, many=True)
+        return Response(serializer.data)
 
 
